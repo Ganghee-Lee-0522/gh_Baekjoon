7a357b8aa12cebb5cef3cecc40c81e52d8492026
Ganghee Lee <gh_5226@naver.com>
2022-07-24 03:11:09 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int year;
+    scanf("%d", &year);
+    
+    if(year%4 == 0)
+    {
+        if(year%100 == 0)
+        {
+            if(year%400 == 0)
+                printf("1");
+            else
+                printf("0");
+        }
+        else
+            printf("1");
+    }
+    else
+        printf("0");
+
+    return 0;
+}
