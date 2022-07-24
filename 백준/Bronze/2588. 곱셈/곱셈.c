acb7dada2fa9b53f69329b05873ce290cbd0bd75
Ganghee Lee <gh_5226@naver.com>
2022-07-24 23:07:47 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    printf("%d\n", a * (b % 10));
+    printf("%d\n", a * ((b / 10) % 10));
+    printf("%d\n", a * (b / 100));
+    printf("%d", a * b);
+    
+    return 0;
+}
