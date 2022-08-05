2a6bbc71c39ff4e3458bb378a7cbc319ec27f698
Ganghee Lee <gh_5226@naver.com>
2022-08-05 14:10:51 +0900

    Update 팩토리얼.c
    반복문 풀이와 재귀함수 풀이를 모두 작성한 코드입니다.

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.c
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.c
@@ -1,3 +1,5 @@
+// 1. 반복문 사용해서 풀기
+
 #include <stdio.h>
 
 int main()
@@ -11,6 +13,29 @@ int main()
     }
     
     printf("%d", cnt);
+    
+    return 0;
+}
+
+
+// 2. 재귀함수 사용해서 풀기
+
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if(n > 1) // 이때 자주 시행되는 조건을 위에 놓을 것! n == 1을 if 조건으로 두었더니 시간 초과 오류가 발생했다.
+        return n * factorial(n - 1);
+    else
+        return 1;
+}
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    
+    printf("%d", factorial(N));
     
     return 0;
 }
