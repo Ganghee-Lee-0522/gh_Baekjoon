39aa0151a97d680ed0b707cb5a6afa2a1a31da44
Ganghee Lee <gh_5226@naver.com>
2022-08-21 23:06:18 +0900

    [Silver V] Title: Base Conversion, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/11576. Base Conversion/Base Conversion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int A, B, m, tmp, arr[25];
+    int N = 0, idx = 0;
+	
+	scanf("%d %d %d", &A, &B, &m);
+	
+	for(int i = 0; i < m; i++){
+		scanf("%d", &tmp);
+		N=(N*A)+tmp;
+	}
+	
+	while(N){
+		arr[idx++] = N % B;
+		N /= B;
+	}
+	for(int i = idx - 1; i >= 0; i--){
+		printf("%d ", arr[i]);
+	}
+	
+	return 0;
+}
